bfa891d91f76c1a709399e51be439a0233117ff3
Chayan <chayanforyou007@gmail.com>
2022-08-15 00:26:23 +0600

    connect to live server

--- app/src/main/java/me/avishek/ebook/constant/Constant.java
+++ app/src/main/java/me/avishek/ebook/constant/Constant.java
@@ -3,5 +3,5 @@ package me.avishek.ebook.constant;
 public class Constant {
 
     //*********** API Base URL ********//
-    public static final String BASE_URL = "http://192.168.0.215:8080/";
+    public static final String BASE_URL = "https://ebook-apps.herokuapp.com/";
 }
